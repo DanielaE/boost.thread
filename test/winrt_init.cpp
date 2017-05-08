2e5043af2317736e7a18fc17285c505caa3aa550
Daniela Engert <dani@ngrt.de>
2017-05-08 12:58:38 +0200

    Suppress pop-up windows from Windows error reporting or JIT debugging.
    Signed-off-by: Daniela Engert <dani@ngrt.de>

--- test/winrt_init.cpp
+++ test/winrt_init.cpp
@@ -16,4 +16,45 @@
 // This is easier than calling in the main function for each test case.
 Microsoft::WRL::Wrappers::RoInitializeWrapper runtime(RO_INIT_MULTITHREADED);
 
+#else
+    
+#if defined(_MSC_VER) && !defined(__CYGWIN__)
+
+extern "C" {
+#ifdef _DEBUG
+__declspec(dllimport) int _CrtSetReportMode(int, int);
+__declspec(dllimport) void* _CrtSetReportFile(int, void*);
+#endif
+__declspec(dllimport) unsigned __stdcall SetErrorMode(unsigned);
+}
+
+namespace boost { namespace detail {
+
+template <typename _>
+struct error_popup_suppressor {
+    error_popup_suppressor() { switch_off_error_popups(); }
+
+    static void switch_off_error_popups() {
+        if (!done) {
+            done = true;
+#ifdef _DEBUG
+            _CrtSetReportMode(1, 1);
+            _CrtSetReportFile(1, (void*)-5);
+#endif
+            SetErrorMode(3);
+        }
+    };
+    static bool done;
+};
+template <typename _>
+bool error_popup_suppressor<_>::done = false;
+
+namespace {
+error_popup_suppressor<bool> _;
+}
+
+}}
+
+#endif
+
 #endif
